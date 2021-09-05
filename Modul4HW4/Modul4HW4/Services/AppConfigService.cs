3949f96977472ff9ea627f3f3d0729458d151f56
Yurii <leoonovn@gmail.com>
2021-09-05 15:08:25 +0300

    Сделал конфиг

---
+++ Modul4HW4/Modul4HW4/Services/AppConfigService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Modul4HW4.Configs;
+
+namespace Modul4HW4.Services
+{
+    public class AppConfigService
+    {
+        public AppConfigService()
+        {
+            var config = GetConfig();
+            ConnectionString = config.ConnectionString;
+        }
+
+        public string ConnectionString { get; }
+
+        private AppConfig GetConfig()
+        {
+            var json = File.ReadAllText("config.json");
+            var config = JsonConvert.DeserializeObject<AppConfig>(json);
+            return config;
+        }
+    }
+}
--- Modul4HW4/Modul4HW4/Starter.cs
+++ Modul4HW4/Modul4HW4/Starter.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Modul4HW4.Services;
 
 namespace Modul4HW4
 {
     public class Starter
     {
+        private AppConfigService _config = new AppConfigService();
         public void Run()
         {
+            Console.WriteLine(_config.ConnectionString);
         }
     }
 }
